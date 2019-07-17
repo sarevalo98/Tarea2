727f0f9d1bc064ee7f561180e747374a975ed624
Default user <sa.arevalo@uniandes.edu.co>
2019-07-17 01:43:16 +0000

    Original y transformada 2d

--- Fourier.py
+++ Fourier.py
@@ -10,7 +10,7 @@ image2= plt.imread("cara_03_grisesMF.png") #feliz
 #transformada de Fourier 2d para las imagenes.
 imagetrans1= fft2(image1)
 imagetrans2= fft2(image2)
-######## Grafique el espectro de Fourier de la imagen. Guarde la grafica sin mostrarla en FFT2D.png
+#imagen original de en escala de grises
 plt.figure()
 plt.subplot(1,2,1)
 plt.imshow(image1,cmap='gray')
@@ -22,7 +22,7 @@ plt.imshow(image2,cmap='gray')
 plt.title("Cara_03")
 plt.colorbar()
 plt.savefig("Original.png")
-
+#Espectro de Fourier de las imagenes
 plt.figure()
 plt.subplot(1,2,1)
 plt.imshow(abs(imagetrans1),norm=LogNorm(),cmap='gray')
@@ -33,4 +33,4 @@ plt.subplot(1,2,2)
 plt.imshow(abs(imagetrans2),norm=LogNorm(),cmap='gray')
 plt.title("Transformada Cara_03")
 plt.colorbar()
-plt.savefig("Transformada.png")
+plt.savefig("Espectro_Fourier.png")
