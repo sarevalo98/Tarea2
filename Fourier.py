7d54cd213c15dc3dd1c46f944d04eb8330dea9e1
Default user <sa.arevalo@uniandes.edu.co>
2019-07-18 20:45:39 +0000

    superposicion HORRIBLE

--- Fourier.py
+++ Fourier.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
-from scipy.fftpack import fft, fftfreq, fft2, fftshift
+from scipy.fftpack import fft, fftfreq, fft2, fftshift,  ifft2
 from matplotlib.colors import LogNorm
 
 #Escala de grises obtenido de https://stackoverflow.com/questions/3823752/display-image-as-grayscale-using-matplotlib
@@ -11,60 +11,79 @@ image2= plt.imread("cara_03_grisesMF.png") #feliz
 #transformada de Fourier 2d para las imagenes.
 imagetrans1= fft2(image1)
 imagetrans2= fft2(image2)
+joker1=imagetrans1
+joker2=imagetrans2
 
 ##frecuencias del espectro de la transformada(.shift)
 ceros1= fftshift(imagetrans1)
 ceros2= fftshift(imagetrans2)
 
-#Filtros
-filtro1=np.where(ceros1<10**3)
-filtro2=np.where(ceros2>10**0)
 
-fil1=ceros1[filtro1]
-fil2=ceros2[filtro2]
 #imagen original de en escala de grises
 plt.figure(figsize=(10,10))
-plt.subplot(2,4,1)
+plt.subplot(3,2,1)
 plt.imshow(image1,cmap='gray')
 plt.title("Cara_02")
 plt.colorbar()
 
-plt.subplot(2,4,2)
+plt.subplot(3,2,2)
 plt.imshow(image2,cmap='gray')
 plt.title("Cara_03")
 plt.colorbar()
 
 #Espectro de Fourier de las imagenes
-plt.subplot(2,4,3)
-plt.imshow(abs(imagetrans1),norm=LogNorm(),cmap='gray')
+plt.subplot(3,2,3)
+plt.imshow(abs(joker1),norm=LogNorm(),cmap='gray')
 plt.title("Espectro transformada Cara02")
 plt.colorbar()
 
-plt.subplot(2,4,4)
-plt.imshow(abs(imagetrans2),norm=LogNorm(),cmap='gray')
+plt.subplot(3,2,4)
+plt.imshow(abs(joker2),norm=LogNorm(),cmap='gray')
 plt.title("Espectro transformada Cara03")
 plt.colorbar()
 
 #frecuencias del espectro
-plt.subplot(2,4,5)
+plt.subplot(3,2,5)
 plt.imshow(abs(ceros1),norm=LogNorm(),cmap='gray')
 plt.title("Frecuencias del espectro Cara_02")
 plt.colorbar()
 
-plt.subplot(2,4,6)
+plt.subplot(3,2,6)
 plt.imshow(abs(ceros2),norm=LogNorm(),cmap='gray')
 plt.title("Frecuencias del espectro Cara_03")
 plt.colorbar()
+plt.savefig("JETAS1.png")
+
+#Filtros
+filtro1=np.where(imagetrans1>50)
+filtro2=np.where(imagetrans2<20)
+imagetrans1[filtro1]=0
+imagetrans2[filtro2]=0
+
+#transformada inversa de las imagenes filtradas
+ffinal1 = ifft2(imagetrans1)
+ffinal2 = ifft2(imagetrans2)
+superpos = np.real(ffinal1)+np.real(ffinal2)
 
 #Filtros de la imagen
-plt.subplot(2,4,7)
-plt.imshow(np.real(fil1),cmap='gray')
+plt.figure(figsize=(10,10))
+plt.subplot(1,2,1)
+plt.imshow(np.abs(imagetrans1),norm=LogNorm(),cmap='gray')
 plt.title("Filtro Cara_02")
 plt.colorbar()
 
-plt.subplot(2,4,8)
-plt.imshow(np.real(fil2),cmap='gray')
+plt.subplot(1,2,2)
+plt.imshow(np.abs(imagetrans2),cmap='gray')
 plt.title("Filtro Cara_03")
 plt.colorbar()
 
-plt.savefig("JETAS.png")
+plt.subplot(1,3,1)
+plt.imshow(np.real(ffinal1),cmap='gray')
+plt.title("Cara_02 filtrada")
+plt.subplot(1,3,2)
+plt.imshow(np.real(ffinal2),cmap='gray')
+plt.title("Cara_03 filtrada")
+plt.subplot(1,3,3)
+plt.imshow(superpos,cmap='gray')
+plt.title("Superposición de las imagenes")
+plt.savefig("JETAS2.png")
