fe3585235daeaf901a1348dea642ecb44fbc7cb5
Default user <sa.arevalo@uniandes.edu.co>
2019-07-18 22:18:29 +0000

    Terminado

--- hw2.mk
+++ hw2.mk
@@ -1,9 +1,8 @@
-ENERGIA_SISTEMA.png MOMENTUM_ANGULAR.png VELOCIDADES.png ORBITAS.png: Plots_hw2.py Euler.dat LeapFrog.dat RungeKutta.dat Euler1.dat LeapFrog1.dat RungeKutta1.dat Euler2.dat LeapFrog2.dat RungeKutta2.dat
+Parte1.png Parte2.png ENERGIA_SISTEMA.png MOMENTUM_ANGULAR.png VELOCIDADES.png ORBITAS.png: Fourier.py Plots_hw2.py Euler.dat LeapFrog.dat RungeKutta.dat Euler1.dat LeapFrog1.dat RungeKutta1.dat Euler2.dat LeapFrog2.dat RungeKutta2.dat
 	python Plots_hw2.py
+	python Fourier.py
 %.dat:a.out
 	./a.out
 a.out: ODEs.cpp
 	g++ ODEs.cpp
-Parte1.png:Fourier.py
-    python Fourier.py
     
