2ead697622b0ad2a5377ba0f2faa386c1c472690
Default user <sa.arevalo@uniandes.edu.co>
2019-07-18 23:43:21 +0000

    Terminado

--- Fourier.py
+++ Fourier.py
@@ -18,44 +18,33 @@ ceros1= fftshift(imagetrans1)
 ceros2= fftshift(imagetrans2)
 
 
-#imagen original de en escala de grises
-plt.figure(figsize=(10,10))
-plt.subplot(3,2,1)
-plt.imshow(image1,cmap='gray')
-plt.title("Cara_02")
-plt.colorbar()
-
-plt.subplot(3,2,2)
-plt.imshow(image2,cmap='gray')
-plt.title("Cara_03")
-plt.colorbar()
-
 #Espectro de Fourier de las imagenes
-plt.subplot(3,2,3)
-plt.imshow(abs(imagetrans1),norm=LogNorm(),cmap='gray')
+plt.figure(figsize=(10,10))
+plt.subplot(1,2,1)
+plt.imshow(abs(imagetrans1),norm=LogNorm())
 plt.title("Espectro transformada Cara02")
 plt.colorbar()
 
-plt.subplot(3,2,4)
-plt.imshow(abs(imagetrans2),norm=LogNorm(),cmap='gray')
+plt.subplot(1,2,2)
+plt.imshow(abs(imagetrans2),norm=LogNorm())
 plt.title("Espectro transformada Cara03")
 plt.colorbar()
+plt.savefig("FFtIm.pdf")
 
 #frecuencias del espectro
-plt.subplot(3,2,5)
-plt.imshow(abs(ceros1),norm=LogNorm(),cmap='gray')
+plt.figure(figsize=(10,10))
+plt.subplot(2,2,1)
+plt.imshow(abs(ceros1),norm=LogNorm())
 plt.title("Frecuencias del espectro Cara_02")
 plt.colorbar()
 
-plt.subplot(3,2,6)
-plt.imshow(abs(ceros2),norm=LogNorm(),cmap='gray')
+plt.subplot(2,2,2)
+plt.imshow(abs(ceros2),norm=LogNorm())
 plt.title("Frecuencias del espectro Cara_03")
 plt.colorbar()
-plt.savefig("Parte1.png")
-
 #Filtros
-filtro1=np.where(ceros1>90)
-filtro2=np.where(ceros2<10)
+filtro1=np.where(abs(ceros1)>40)
+filtro2=np.where(abs(ceros2)<90)
 ceros1[filtro1]=0
 ceros2[filtro2]=0
 
@@ -65,15 +54,18 @@ ffinal2 = ifftshift(ceros2)
 supe1 = ifft2(ffinal1)
 supe2 = ifft2(ffinal2)
 imafinal=supe1 + supe2
-#Filtros de la imagen
-plt.figure(figsize=(10,10))
-plt.subplot(1,3,1)
-plt.imshow(np.real(supe1),cmap='gray')
+
+plt.subplot(2,2,3)
+plt.imshow(abs(supe1),cmap='gray')
 plt.title("Cara_02 filtrada")
-plt.subplot(1,3,2)
-plt.imshow(np.real(supe2),cmap='gray')
+plt.colorbar()
+plt.subplot(2,2,4)
+plt.imshow(abs(supe2),cmap='gray')
 plt.title("Cara_03 filtrada")
-plt.subplot(1,3,3)
+plt.colorbar()
+plt.savefig("ImProceso.pdf")
+
+plt.figure()
 plt.imshow(abs(imafinal),cmap='gray')
 plt.title("Superposición de las imagenes")
-plt.savefig("Parte2.png")
+plt.savefig("ImHybrid.pdf")
