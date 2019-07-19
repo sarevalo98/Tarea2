a8d9dc3cbe8bc2c4a186d3c809e7cab14760f21d
Default user <sa.arevalo@uniandes.edu.co>
2019-07-19 00:20:07 +0000

    Terminado

--- hw2.mk
+++ hw2.mk
@@ -1,4 +1,4 @@
-Parte1.png Parte2.png ENERGIA_SISTEMA.png MOMENTUM_ANGULAR.png VELOCIDADES.png ORBITAS.png: Fourier.py Plots_hw2.py Euler.dat LeapFrog.dat RungeKutta.dat Euler1.dat LeapFrog1.dat RungeKutta1.dat Euler2.dat LeapFrog2.dat RungeKutta2.dat
+FFtIm.pdf ImProceso.pdf ImHybrid.pdf Ener_met_dt.pdf XY_met_dt.pdf VxVy_met_dt.pdf Mome_met_dt.pdf: Fourier.py Plots_hw2.py Euler.dat LeapFrog.dat RungeKutta.dat Euler1.dat LeapFrog1.dat RungeKutta1.dat Euler2.dat LeapFrog2.dat RungeKutta2.dat
 	python Plots_hw2.py
 	python Fourier.py
 %.dat:a.out
